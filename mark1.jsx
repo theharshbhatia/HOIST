4839283b6508838f9f90c49a08fa119baaf0d09f
Hash113 <harshbhatia296@gmail.com>
2013-10-07 10:38:53 +0530

    sitting change commit

--- mark1.jsx
+++ mark1.jsx
@@ -1,2 +1,26 @@
-//alert the document name
-alert(app.documents[0]);
+//Photoshop information retrival code
+
+//Document information
+var Document_Name = app.documents[0]; //later the title tag of webpage
+var Document_Length=app.activeDocument.height;
+var Document_Width=app.activeDocument.width;
+
+// Layergroup Information
+var LayerSets_Numbers =activeDocument.layerSets.length; //No of layer groups present in the ps document
+//alert(activeDocument.layerSets[0].artLayers.length);//length of a particular layerset
+
+//Layer Information
+var Layer_Numbers = app.activeDocument.layers.length // No of layers in document
+
+//var myLayer = activeDocument.layerSets[0].artLayers[0]; //first layer of first group
+// alert(activeDocument.layers[0]); //Name of first layer in the document
+
+//Listing all layers
+
+for (var i=0;i<Layer_Numbers;i++)
+{
+var myLayer = activeDocument.layers[i];
+alert(myLayer.name);	
+}
+
+//alert(myLayer.name);
