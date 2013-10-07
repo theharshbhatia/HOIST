4641527f36b5ad7dbaef1642f7b30287a5f4f222
Hash113 <harshbhatia296@gmail.com>
2013-10-08 00:15:12 +0530

    open text field found

--- mark1.jsx
+++ mark1.jsx
@@ -4,23 +4,51 @@
 var Document_Name = app.documents[0]; //later the title tag of webpage
 var Document_Length=app.activeDocument.height;
 var Document_Width=app.activeDocument.width;
+var Document_Path=app.activeDocument.fullName; //full path of the ps document
+//alert(activeDocument.info.author)
+//---------------------------------------------------------
 
 // Layergroup Information
 var LayerSets_Numbers =activeDocument.layerSets.length; //No of layer groups present in the ps document
-//alert(activeDocument.layerSets[0].artLayers.length);//length of a particular layerset
+//alert(activeDocument.layerSets[0].artLayers.length); //length of a particular layerset
 
+//---------------------------------------------------------
 //Layer Information
-var Layer_Numbers = app.activeDocument.layers.length // No of layers in document
+var Independent_Layer_Numbers = app.activeDocument.layers.length // No of layers independent in document (including numbers of free layers n groups)
 
 //var myLayer = activeDocument.layerSets[0].artLayers[0]; //first layer of first group
 // alert(activeDocument.layers[0]); //Name of first layer in the document
 
-//Listing all layers
 
-for (var i=0;i<Layer_Numbers;i++)
+//alert(layerRef = app.activeDocument.artLayers[0].kind); //property of a layer
+
+//Listing all layers
+var Layers = new Array();
+
+for (var i=0;i<3;i++)
 {
-var myLayer = activeDocument.layers[i];
-alert(myLayer.name);	
+Layers[i] = activeDocument.layers[i].name;
+// alert(Layers[i]);	
 }
 
+
+for (var j=0; j<18;j++)
+{
+	if(activeDocument.layers[j].kind)
+	{
+		if (activeDocument.layers[j].kind=="LayerKind.TEXT")
+		{
+			alert("yaa i got a text field");
+
+		}
+		
+	}
+	else
+	{
+		alert("layerset");
+		
+	}
+}
+
+//alert(Layers);
 //alert(myLayer.name);
