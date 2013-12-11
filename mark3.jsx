46c5ecbc21dbad5ea23bf8a24a27f84edaf5bc2a
Hash113 <harshbhatia296@gmail.com>
2013-12-12 04:00:21 +0530

    paragraph field added

--- mark3.jsx
+++ mark3.jsx
@@ -43,12 +43,44 @@ function add_page_close(){
 	html_code+="</body></html>";
 }
 
-function find_no_of_layersets(){
-	layerset_array = new Array();
+function layersets(){
+	layer_array = new Array();
 	numoflayerset=activeDocument.layerSets.length;
-	div_name=activeDocument.layerSets[0].name;
-	add_div(div_name);
-	return numoflayerset;
+	// var i=0;
+	for(var i=0;i<numoflayerset;i++)
+	{
+		div_name=activeDocument.layerSets[i].name;
+		add_div(div_name);
+		inside_layer_numbers=activeDocument.layerSets[i].layers.length;
+		// alert(inside_layer);
+		for(var j=0;j<inside_layer_numbers;j++)
+		{
+			layer_name=activeDocument.layerSets[i].layers[j].name;
+			layer_kind=activeDocument.layerSets[i].layers[j].kind;
+			layer_post_kind(layer_kind,i,j);
+		}
+		add_div_close();
+	}
+}
+
+function layer_post_kind(layer_kind,i,j)
+{
+	if(layer_kind=="LayerKind.NORMAL")
+	{
+	//normal layer fucntions 
+
+	}
+	else if (layer_kind=="LayerKind.TEXT") 
+	{
+		 text_layer_addition(i,j);
+	}
+
+}
+
+function text_layer_addition(i,j){
+	html_code+='<p>'+activeDocument.layerSets[i].layers[j].textItem.contents+"</p>";
+	// alert(activeDocument.layerSets[i].layers[j].textItem.font);
+	//alert(j);
 }
 
 function code_chain_begin()
@@ -57,10 +89,12 @@ add_html();
 add_head();
 add_title();
 add_body();
-no=find_no_of_layersets();
-html_code+=no;
-alert(html_code);
+
+layersets();
+
 add_page_close();
+alert(html_code);
+
 }
 
 code_chain_begin();
